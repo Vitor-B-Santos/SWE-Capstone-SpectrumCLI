67dd91b14030ee9b643889d3991fe1a61577f487
c-floressantos <christopher.floressantos@gmail.com>
2023-10-11 02:48:08 -0400

    more formatting
    added .gitignore
    added requirements.txt

--- src/index.py
+++ src/index.py
@@ -9,15 +9,15 @@ capture = cv2.VideoCapture("CW Signal.mp4")
 object_detect = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 while True:
-        ret, frame = capture.read()
+    ret, frame = capture.read()
 
-        mask = object_detect.apply(frame)
-       
-        cv2.imshow('Mask', mask)
+    mask = object_detect.apply(frame)
 
-        key = cv2.waitKey(30)
-        if key == 27:
-                break
-        
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
 capture.release()
 cv2.destroyAllWindows()
