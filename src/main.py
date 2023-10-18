4b4448ee1745a70ee10681ed36f41acceaadead9
Treyredden <Treyredden@ymail.com>
2023-10-18 14:06:14 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,36 +1,52 @@
 from argparse import ArgumentParser, Namespace
 import os
 import argparse
+import shutil
 
 
-#Define a function to validate the file type
+# Define a function to validate the file type
 def is_valid_mp4_file(filename):
-    #check if the file is an mp4 file
+    # check if the file is an mp4 file
     if not filename.lower().endswith(".mp4"):
         raise argparse.ArgumentTypeError("Input file must have a '.mp4 extension")
     return filename
 
-#create the main parser argument
-parser = ArgumentParser(description="CLI for working analizing Spectrum Analizer mp4 files")
 
-#create subparsers obj for handling commands
+# create the main parser argument
+parser = ArgumentParser(
+    description="CLI for working analizing Spectrum Analizer mp4 files"
+)
+
+# create subparsers obj for handling commands
 subparsers = parser.add_subparsers(dest="command", title="Commands")
 
 # Add an argument for the input MP4 file with the validation function
 read_parser = subparsers.add_parser("r", help="Read an MP4 video file")
-read_parser.add_argument("input_file", type=is_valid_mp4_file, help="path to the imput mp4 file.")
+read_parser.add_argument(
+    "input_file", type=is_valid_mp4_file, help="path to the imput mp4 file."
+)
 
-#Add an argument for the processing of an MP4 file with validation
+# Add an argument for the processing of an MP4 file with validation
 process_parser = subparsers.add_parser("p", help="Process an MP4 Video file")
-process_parser.add_argument("input_file", type=is_valid_mp4_file, help="Path to the input MP4 file")
+process_parser.add_argument(
+    "input_file", type=is_valid_mp4_file, help="Path to the input MP4 file"
+)
 
 help_parser = subparsers.add_parser("h", help="Display help information for CLI")
-#parse the command-line arguments
+# parse the command-line arguments
 args: Namespace = parser.parse_args()
 
-#Preform the action based on the given subcommand
+# Preform the action based on the given subcommand
 if args.command == "r":
     print(f"Input file '{args.input_file}' is accepted.")
+
+    videos_dir = os.path.join("src", "videos")
+    if not os.path.exists(videos_dir):
+        os.makedirs(videos_dir)
+
+    destination = os.path.join(videos_dir, os.path.basename(args.input_file))
+    shutil.copy(args.input_file, destination)
+    print(f"Video copied to '{destination}'")
 
 elif args.command == "p":
     print(f"Input file '{args.input_file}' is processing...")
@@ -38,5 +54,5 @@ elif args.command == "h":
     parser.print_help()
 
 else:
-    #handle case where no subcommand is provided
+    # handle case where no subcommand is provided
     print("No subcommand specified. Please try again or use help")
