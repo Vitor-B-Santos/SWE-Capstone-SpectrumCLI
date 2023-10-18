90583d593e9f2cbaa2ee23db757d8d1a319fd7d7
Sam Corella <samcorella77@gmail.com>
2023-10-18 14:34:56 -0400

    Add canny to main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, Namespace
 import os
 import argparse
 import shutil
+import cv2
 
 
 # Define a function to validate the file type
@@ -50,6 +51,35 @@ if args.command == "r":
 
 elif args.command == "p":
     print(f"Input file '{args.input_file}' is processing...")
+    
+    capture = cv2.VideoCapture("./videos/CW Signal.mp4")
+
+    # Tweak and test these
+    lower = 200   # Lower threshold value
+    upper = 300  # Upper threshold value
+
+    while True:
+            ret, frame = capture.read()
+
+            # Crop Video
+            # The frame is treated as a 2D array of pixels.
+            # Using array slicing, we specify row_start:row_end, column_start:column_end.
+            # Essentially, the top right corner of the video is the coordinate
+            # (row_start, column_start) and the bottom right corner is the
+            # coordinate (row_end, column_end).
+            crop = frame[150:1000, 700:1600]
+
+            edge = cv2.Canny(crop, lower, upper)
+
+            cv2.imshow('Edge', edge)
+
+            key = cv2.waitKey(30)
+            if key == 27:
+                    break
+            
+    capture.release()
+    cv2.destroyAllWindows()
+
 elif args.command == "h":
     parser.print_help()
 
